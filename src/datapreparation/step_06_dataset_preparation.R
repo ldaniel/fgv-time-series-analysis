0068322db3e9ac568ed7a069753d7a2af3802b14
ldaniel <contato@leandrodaniel.com>
2019-10-09 18:01:23 -0300

    Refactoring existent files

--- src/datapreparation/step_06_dataset_preparation.R
+++ src/datapreparation/step_06_dataset_preparation.R
@@ -10,3 +10,13 @@ selic_target$Value  <- selic_target$Value / 100
 ## filter IPCA data -----
 ipca_12m        <- filter(ipca_12m, Date >= make_date(2002, 1, 1))
 ipca_12m$Value  <- ipca_12m$Value / 100
+
+## saving processed dataset -----
+
+# remover este comentário e as 2 linhas abaixo depois que tiver o dataset final
+# library(readxl)
+# target_dataset <- read_excel("D:/OneDrive/FGV/04_Modulos/08 - Analise de Series Temporais/Trabalho Individual/DepartmentStoreSales_V2.xls")
+
+if (!file.exists('./data/processed/target_dataset.rds')) {
+  saveRDS(target_dataset, './data/processed/target_dataset.rds')
+}
