19cc2a9fab2a0d0dbe055112d63aab79bb8c9214
RodriGonca <rodrigo.goncalves@me.com>
2019-10-10 21:52:15 -0300

    Rodrigo Testing on Tesouro Direto Time Series

--- src/datapreparation/step_06_dataset_preparation.R
+++ src/datapreparation/step_06_dataset_preparation.R
@@ -15,3 +15,24 @@ ipca_12m$Value  <- ipca_12m$Value / 100
 # if (!file.exists('./data/processed/target_dataset.rds')) {
 #   saveRDS(target_dataset, './data/processed/target_dataset.rds')
 # }
+
+# Create Time Series ----------------------------------------------------------
+
+df <- filter(bond_data, titulo == "NTN-B Princ 2024-08-15") %>% 
+  select(dia, taxa_venda) %>% 
+  arrange(dia)
+
+dates <- seq(min(df$dia), max(df$dia), by = "day") %>% as_tibble
+colnames(dates) <- c('dia')
+
+df <- left_join(dates, df, by = 'dia')
+df$isWeekend <- ifelse(wday(df$dia) %in% c(7, 1), 1, 0)
+df$isHoliday <- ifelse(df$dia %in% holidaysANBIMA, 1, 0)
+df$taxa_venda = na.locf(df$taxa_venda, na.rm = TRUE) * 100
+
+df$date <- make_date(year(df$dia), month(df$dia), 1)
+ 
+df <- group_by(df, date) %>%
+   summarise(taxa_venda = mean(taxa_venda))
+ 
+saveRDS(df, 'data/processed/target_dataset.rds')
