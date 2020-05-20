f49197007d2b8f90b3c577ac3a75dcb79b7babb4
RodriGonca <rodrigo.goncalves@me.com>
2020-05-20 11:27:31 -0300

    adding binder requirements

---
+++ install.R
@@ -0,0 +1,19 @@
+install.packages('Quandl')
+
+# data prep libraries
+install.packages('tibble')
+install.packages('magrittr')
+install.packages('dplyr')
+install.packages('tidyr')
+install.packages('readr')
+install.packages('readxl')
+install.packages('stringr')
+install.packages('lubridate')
+install.packages('reticulate')
+install.packages('bizdays')
+install.packages('gifski')
+
+# plotting libraries
+install.packages('ggplot2')
+install.packages('plotly')
+install.packages('ggthemes')
