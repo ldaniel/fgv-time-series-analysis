d3c950c6cb137acbe390026a6649166d4d2924e1
Rodrigo Goncalves <50252438+RodriGonca@users.noreply.github.com>
2020-05-21 14:20:24 -0300

    Update step_06_dataset_preparation.R

--- src/datapreparation/step_06_dataset_preparation.R
+++ src/datapreparation/step_06_dataset_preparation.R
@@ -30,4 +30,12 @@ df$date <- make_date(year(df$dia), month(df$dia), 1)
 df <- group_by(df, date) %>%
    summarise(taxa_venda = mean(taxa_venda))
  
-saveRDS(df, 'data/processed/target_dataset.rds')
+result = tryCatch({
+  saveRDS(df, 'data/processed/target_dataset.rds')
+}, warning = function(w) {
+  
+}, error = function(e) {
+  saveRDS(df, '../data/processed/target_dataset.rds')
+}, finally = {
+  
+})
